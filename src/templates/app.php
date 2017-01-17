2ef290cda97429daf2faacc5bec93476823c3500
fatfaldog <fatfaldog@gmail.com>
2017-01-17 22:02:34 +0300

    simplify markup, faster rendering, better appearance

--- src/templates/app.php
+++ src/templates/app.php
@@ -9,12 +9,25 @@
     </head>
     <body>
         <main class="container">
-            <ul class="list-group col-md-8">
-            <?php foreach($data['products'] as $product): ?>
-                <li class="list-group-item">
-                    <div class="panel panel-default">
-                        <div class="panel-heading"><?= $product['name'] ?></div>
-                        <div class="panel-body">
+            <h1 class="text-center">Test <a href="http://telegra.ph/Fullstack-developer-test-01-12">challenge</a> for fullstack developer</h1>
+            <div class="col-xs-8">
+                <h2>Filtered product list here</h2>
+                <?php foreach($data['products'] as $product): ?>
+                <div class="panel panel-default">
+                    <div class="panel-heading"><?= $product['name'] ?></div>
+                    <div class="panel-body">
+                        <div class="col-xs-4">
+                            <p>Some info about product here:</p>
+                            <p>
+                                Image: <span class="glyphicon glyphicon-camera"></span><br>
+                                Description: some description<br>
+                                Price: xxxx $<br>
+                                Availability: yy units<br>
+                                Anything else
+                            </p>
+                        </div>
+                        <div class="col-xs-8">
+                            <p>Product properties here:</p>
                             <ul class="list-group">
                             <?php foreach($product['props'] as $property): ?>
                                 <li class="list-group-item">
@@ -24,39 +37,35 @@
                             </ul>
                         </div>
                     </div>
-                </li>
-            <?php endforeach; ?>
-            </ul>
-            <form id="products-filter">
-                <ul class="list-group col-md-4">
-                    <?php foreach($data['groups'] as $group): ?>
-                    <li class="list-group-item">
-                        <div class="panel panel-default">
-                            <div class="panel-heading"><?= $group['name'] ?></div>
-                            <div class="panel-body">
-                                <ul class="list-group">
-                                <?php foreach($group['props'] as $property): ?>
-                                    <li class="list-group-item">
-                                        <label>
-                                            <input
-                                                type="checkbox"
-                                                name="filter[prop][<?= $property['id'] ?>]"
-                                                value="true"
-                                                <?= $property['available'] || $group['selected'] ? '': 'disabled="disabled"' ?>
-                                                <?= $property['selected'] ? 'checked="checked"' : '' ?>
-                                            >
-                                            <?= $property['name'] ?>
-                                        </label>
-                                    </li>
-                                <?php endforeach; ?>
-                                </ul>
-                            </div>
-                        </div>
-                    </li>
+                </div>
                 <?php endforeach; ?>
-                </ul>
             </div>
-        </aside>
+            <form id="products-filter" class="col-xs-4">
+                <h2>Filters here</h2>
+                <?php foreach($data['groups'] as $group): ?>
+                <div class="panel panel-default">
+                    <div class="panel-heading"><?= $group['name'] ?></div>
+                    <div class="panel-body">
+                        <ul class="list-group">
+                        <?php foreach($group['props'] as $property): ?>
+                            <li class="list-group-item">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="filter[prop][<?= $property['id'] ?>]"
+                                        value="true"
+                                        <?= $property['available'] || $group['selected'] ? '': 'disabled="disabled"' ?>
+                                        <?= $property['selected'] ? 'checked="checked"' : '' ?>
+                                    >
+                                    <?= $property['name'] ?>
+                                </label>
+                            </li>
+                        <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </form>
         </main>
         <!-- bootstrap JS CDN -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
